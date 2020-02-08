bd27aa54f80cbd9a16ba121c246c193843f73628
Andrew Dimmer <contactandrewd@gmail.com>
2020-02-08 17:13:52 -0500

    feat: Duck Now Quacks!
    At least, on the webpage. Still need to add the Firebase handler to the Hover code.

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import MainPage from "./components/MainPage";
+import QuackPage from "./components/QuackPage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const url = window.location.href.toLowerCase();
+if (url.indexOf("demo") > 0) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+} else if (url.indexOf("quack") > 0) {
+  ReactDOM.render(<QuackPage />, document.getElementById("root"));
+} else {
+  ReactDOM.render(<MainPage />, document.getElementById("root"));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
