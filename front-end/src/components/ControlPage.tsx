bed2a5d9e6986b11e9b64759b17417648076d694
James Lynott <41302574+jcelynott@users.noreply.github.com>
2020-02-08 21:26:37 -0500

    Added Concentration page

--- front-end/src/components/ControlPage.tsx
+++ front-end/src/components/ControlPage.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { Typography, Button } from "@material-ui/core";
 import { firebaseApp, UserData } from "../firebase/config";
 // import PairDucksModule from "./Modules/PairDuckModule";
+import Concentration from "../components/Modules/Concentration";
 declare interface ControlPageProps {
   userId: string;
   userData: UserData;
@@ -20,6 +21,7 @@ const ControlPage: React.FunctionComponent<ControlPageProps> = ({
         userId={userId}
         myDucks={userData.ducks}
       ></PairDucksModule>*/}
+      <Concentration userID={userId} userData={userData} />
       <Button onClick={handleLogout}> Log Out </Button>
     </Fragment>
   );
---
+++ front-end/src/components/Modules/Budget.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import { Typography, Button, TextField } from "@material-ui/core";
+import {
+  FinanceSettings,
+  ConcentrationSettings,
+  UserData,
+  firebaseApp
+} from "../../firebase/config";
+import { User } from "firebase";
+import { updateConcentrationMode } from "../../firebase/user";
+
+const BudgetPage: React.FunctionComponent = () => {
+  return <Fragment></Fragment>;
+};
+
+export default BudgetPage;
