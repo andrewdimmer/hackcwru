ef91f1e184203e3e975156f1d0ce5e390e58231a
Broski-AC <acbroski@oakland.edu>
2020-02-08 21:25:25 -0500

    Updated DemoPage with Popup

--- front-end/src/components/DemoPage.tsx
+++ front-end/src/components/DemoPage.tsx
@@ -5,13 +5,18 @@ import {
   createStyles,
   makeStyles,
   Theme,
-  Grid
+  Grid,
+  Modal,
+  TextField,
+  FormControl,
+  InputAdornment
 } from "@material-ui/core";
 import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
 import DateRangeIcon from "@material-ui/icons/DateRange";
 import RestoreIcon from "@material-ui/icons/Restore";
 import LinkOffIcon from "@material-ui/icons/LinkOff";
 import HearingIcon from "@material-ui/icons/Hearing";
+import ForwardIcon from "@material-ui/icons/Forward";
 
 declare interface DemoProps {}
 const DemoPage: React.FunctionComponent<DemoProps> = ({}) => {
@@ -24,11 +29,40 @@ const DemoPage: React.FunctionComponent<DemoProps> = ({}) => {
       title: {
         color: "#388e3c",
         "font-size": "5vw"
+      },
+      modelMask: {
+        position: "absolute",
+
+        top: "50vh",
+        left: "50vw", //Need to fix once items are included
+
+        "margin-top": "-10%",
+
+        "margin-left": "-20%",
+        height: "20%",
+        width: "40%",
+        backgroundColor: theme.palette.background.paper,
+        border: "2px solid #000",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3)
+      },
+
+      inputbox: {
+        width: "80%"
       }
     })
   );
 
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Fragment>
@@ -48,9 +82,36 @@ const DemoPage: React.FunctionComponent<DemoProps> = ({}) => {
             color="secondary"
             className={classes.button}
             startIcon={<MonetizationOnIcon />}
+            onClick={handleOpen}
           >
             Pay Amount
           </Button>
+          <Modal
+            aria-labelledby="pay-amount-model"
+            aria-describedby="pay-amount-popup"
+            open={open}
+            onClose={handleClose}
+          >
+            <div className={classes.modelMask}>
+              <h2>Please Insert How Much You Have Spent Today</h2>
+              <TextField
+                className={classes.inputbox}
+                label="Dollar Amount"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">$</InputAdornment>
+                  )
+                }}
+                variant="filled"
+              />
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+                startIcon={<ForwardIcon />}
+              ></Button>
+            </div>
+          </Modal>
           <Button
             variant="contained"
             color="primary"
@@ -75,13 +136,14 @@ const DemoPage: React.FunctionComponent<DemoProps> = ({}) => {
           >
             Reset
           </Button>
+
           <Button
             variant="contained"
             color="secondary"
             className={classes.button}
             startIcon={<LinkOffIcon />}
           >
-            Unpair Duck
+            Unpair All Ducks
           </Button>
         </Grid>
       </div>
