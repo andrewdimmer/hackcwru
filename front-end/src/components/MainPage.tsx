117f4f5232ab9f01157fe266fae33f7e9e451d90
James Lynott <41302574+jcelynott@users.noreply.github.com>
2020-02-09 02:58:56 -0500

    Revert "Revert "Revert "Update to main page login"""
    This reverts commit 588bc8b6296b5fb2a0bdf6577e113a919d6c3c9e.

--- front-end/src/components/MainPage.tsx
+++ front-end/src/components/MainPage.tsx
@@ -1,21 +1,10 @@
 import React, { Fragment, useState } from "react";
 import { User } from "firebase";
-import {
-  Typography,
-  Button,
-  makeStyles,
-  Theme,
-  ButtonBase,
-  withStyles,
-  Card,
-  CardContent,
-  CardActions
-} from "@material-ui/core";
+import { Typography, Button } from "@material-ui/core";
 import ControlPage from "../../src/components/ControlPage";
 import LoginPage from "./LoginPage";
 import { firebaseApp, UserData } from "../firebase/config";
 import { getCreateUser } from "../firebase/user";
-import { findByLabelText } from "@testing-library/react";
 
 const MainPage: React.FunctionComponent = () => {
   const [userId, setUserId] = useState("");
@@ -44,44 +33,6 @@ const MainPage: React.FunctionComponent = () => {
     setUserData(null);
     firebaseApp.auth().signOut();
   };
-
-  //Taken off of the Material-ui.com website, from their complex button example
-  const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-      //minWidth: 275,
-      display: "flex",
-
-      "justify-content": "center",
-
-      "flex-direction": "column",
-
-      "align-items": "center",
-
-      "flex-wrap": "wrap"
-    },
-
-    title: {
-      fontSize: 14
-    },
-
-    pos: {
-      marginBottom: 12
-    }
-  }));
-
-  const classes = useStyles();
-
-  const StyledButton = withStyles({
-    root: {
-      "background-image": "linear-gradient(red, yellow, blue)",
-      height: "90%",
-      width: "60%",
-      color: "blue"
-    },
-    label: {
-      testTransform: "capitalize"
-    }
-  })(Button);
 
   firebaseApp.auth().onAuthStateChanged(user => {
     if (user) {
@@ -125,43 +76,10 @@ const MainPage: React.FunctionComponent = () => {
           )}
         </Fragment>
       )}
-      {/* <Button
-          variant="contained"
-          onClick={handleLoginButton}
-          className={classes.login}
-          color="primary"
-        >
-          Click here to login!
-        </Button> */}
       {!userId && !isLoggingIn && (
-        <span className={classes.root}>
-          <Card className={classes.root} variant="outlined">
-            <CardContent className={classes.root}>
-              <Typography
-                className={classes.title}
-                color="textSecondary"
-                gutterBottom
-              >
-                The Buck Duck Demonstration
-              </Typography>
-              <Typography variant="h3" component="h3" color="primary">
-                A Project Inspired By a Love of Ducks and Technology
-              </Typography>
-              <Typography className={classes.pos} color="secondary">
-                Andrew Dimmer, Nathan Dimmer, James Lynott, Allison Broski
-              </Typography>
-              <Typography variant="body2" component="p" color="textPrimary">
-                Walking talking what's going on. Come up with something that
-                doesn't sound wrong
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <StyledButton variant="contained" onClick={handleLoginButton}>
-                Click Here to Login!
-              </StyledButton>
-            </CardActions>
-          </Card>
-        </span>
+        <Button variant="contained" onClick={handleLoginButton}>
+          Click here to login!
+        </Button>
       )}
     </Fragment>
   );
